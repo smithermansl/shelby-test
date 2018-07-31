c2e331bb0e29a71be8ca10f2b196e06ae913247c
Willis Plummer <willisplummer@gmail.com>
2018-07-31 18:00:17 -0400

    App.js

--- src/App.js
+++ src/App.js
@@ -6,42 +6,10 @@ const App = () =>
       <h1>Welcome to the Clark Take-Home Code Challenge</h1>
     </header>
     <p>
-      Take a look at <code>src/data.js</code>. This is an approximation of what normalized billing data from our API looks like.
+      To get started, make some changes to this component and then save.
     </p>
     <p>
-      For this exercise, we'd like you to do the following:
-    </p>
-    <ul>
-      <li>
-        calculate the total amount billed in dollars
-      </li>
-      <li>
-        calculate the total amount paid in dollars
-      </li>
-      <li>
-        calculate the total amount overdue (unpaid) in dollars
-      </li>
-      <li>
-        calculate the total amount outstanding (not due yet, not paid yet) money in dollars.
-      </li>
-      <li>
-        render these values to the screen along with an ordered list of the bills sorted by date and by category (outstanding, overdue, paid) - see the attached screenshot for reference.
-      </li>
-    </ul>
-    <p>
-      Please test your work as you see fit, use git commits, and if you're comfortable use Flow to add type annotations.
-    </p>
-    <p>
-      Feel free to use any resources and packages available to you, including <a href="https://github.com/hiclark/clark-utils">clark-utils</a>.
-    </p>
-    <p>
-      When you've finished, zip your completed challenge and email it back to us.
-    </p>
-    <p>
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-    <p>
-      If you have any questions, please reach out!
+      The page should reload to reflect your changes!
     </p>
   </div>
 
